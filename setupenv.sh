25f1c803a6600c10fa30373201a0e9736b2d11d6
Luke <stanford.luke.nz@gmail.com>
2017-04-09 11:59:07 +1200

    install some useful packages for linux

---
+++ setupenv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+apt install kupfer
+apt install tmux
+apt install vim
+apt install fish
+apt install git
+apt install virtualenv
+apt install pathogen
+apt install curl
+apt install guake
